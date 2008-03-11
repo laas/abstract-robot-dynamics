933b7c7bd07cf0ab7b6debb59ce4e74a21460ee9
florent <florent@dhcpt1253.a02.aist.go.jp>
2008-03-11 14:27:09 +0900

    Added recommended commands in dedicated page.

--- documentation/additionalDoc/commands.h
+++ documentation/additionalDoc/commands.h
@@ -2,43 +2,117 @@
    \page abstractRobotDynamics_commands Recommended commands in implementations
 
    In this page, we provide a list of commands that we recommend
-   implementations to support in order to keep some compatibility.
+   implementations to support in order to keep some compatibility between different implementations:
+   See CjrlDynamicRobot paragraph "Control of the implementation" for details.
    
    \htmlonly
-      <table width=100% cellpadding=5 cellspacing=0>
-	<tr><td align=center color:#0066CC>
-	    <h4>Name</h4>
-	  </td>
-	  <td align=center>
-	    <h4>Value</h4>
-	  </td>
-	  <td align=center>
-	    <h4>Description</h4>
-	  </td>
-	</tr>
-	<tr><td width="30">
-	    "ComputeZMP"
-	  </td>
-	  <td>
-	    "true"
-	  </td>
-	  <td>
-	    Requires computation of ZMP when calling CjrlDynamicRobot::computeForwardKinematics().
-	  </td>
-	</tr>
-	<tr><td width="30">
-	    "ComputeZMP"
-	  </td>
-	  <td>
-	    "false"
-	  </td>
-	  <td>
-	    Do not require computation of ZMP when calling CjrlDynamicRobot::computeForwardKinematics().
-	  </td>
-	</tr>
-      </table>
+      <div class="memitem">
+	<div class="memproto">
+	  <table>
+	    <tr>
+	      <td><h4>Name</h4></td>
+	      <td>&nbsp;</td>
+	      <td><h4>Value</h4></td>
+	      <td>&nbsp;</td>
+	      <td><h4>Description</h4></td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeZMP"</td>
+	      <td>&nbsp;</td>
+	      <td>"true"</td>
+	      <td>&nbsp;</td>
+	      <td>Require computation of ZMP.</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeZMP"</td>
+	      <td>&nbsp;</td>
+	      <td>"false"</td>
+	      <td>&nbsp;</td>
+	      <td>Do not require computation of ZMP.</td>
+	    </tr>
+	    <tr><td>&nbsp;</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeVelocity"</td>
+	      <td>&nbsp;</td>
+	      <td>"true"</td>
+	      <td>&nbsp;</td>
+	      <td>Require computation of velocities of bodies.</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeVelocity"</td>
+	      <td>&nbsp;</td>
+	      <td>"false"</td>
+	      <td>&nbsp;</td>
+	      <td>Do not require computation of velocities of bodies.</td>
+	    </tr>
+	    <tr><td>&nbsp;</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeCoM"</td>
+	      <td>&nbsp;</td>
+	      <td>"true"</td>
+	      <td>&nbsp;</td>
+	      <td>Require computation of robot center of mass.</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeCoM"</td>
+	      <td>&nbsp;</td>
+	      <td>"false"</td>
+	      <td>&nbsp;</td>
+	      <td>Do not require computation of robot center of mass.</td>
+	    </tr>
+	    <tr><td>&nbsp;</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeAccelerationCoM"</td>
+	      <td>&nbsp;</td>
+	      <td>"true"</td>
+	      <td>&nbsp;</td>
+	      <td>Require computation of robot center of mass acceleration.</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeAccelerationCoM"</td>
+	      <td>&nbsp;</td>
+	      <td>"false"</td>
+	      <td>&nbsp;</td>
+	      <td>Do not require computation of robot center of mass acceleration.</td>
+	    </tr>
+	    <tr><td>&nbsp;</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeMomentum"</td>
+	      <td>&nbsp;</td>
+	      <td>"true"</td>
+	      <td>&nbsp;</td>
+	      <td>require computation of global momentum and momentum of each body.</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeMomentum"</td>
+	      <td>&nbsp;</td>
+	      <td>"false"</td>
+	      <td>&nbsp;</td>
+	      <td>Do not require computation of global momentum and momentum of each body.</td>
+	    </tr>
+	    <tr><td>&nbsp;</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeBackwardDynamics"</td>
+	      <td>&nbsp;</td>
+	      <td>"true"</td>
+	      <td>&nbsp;</td>
+	      <td>Require computation of forces and torques on each joint.</td>
+	    </tr>
+	    <tr>
+	      <td>"ComputeBackwardDynamics"</td>
+	      <td>&nbsp;</td>
+	      <td>"false"</td>
+	      <td>&nbsp;</td>
+	      <td>Do not require computation of forces and torques on each joint.</td>
+	    </tr>
+	  </table>
+	</div>
+      </div>
    \endhtmlonly
-
-   
 
 */
