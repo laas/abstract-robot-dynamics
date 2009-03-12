4078e8223172b8fa24ae8daa9041f0c5498f5774
florent <Florent Lamiraux florent@laas.fr>
2009-03-12 14:55:37 +0900

    Fixed installation by cmake
      Removed autotools related files,
      Fixed abstractRobotDynamics.pc file
      Install abstractRobotDynamics.doxytag

--- abstractRobotDynamics.pc.cmake
+++ abstractRobotDynamics.pc.cmake
@@ -1,9 +1,9 @@
 prefix=${CMAKE_INSTALL_PREFIX}
-exec_prefix=${install_pkg_prefix}/bin
-libdir=${install_pkg_prefix}/lib
+exec_prefix=${install_pkg_prefix}
+libdir=${install_pkg_exec_prefix}/lib
 includedir=${install_pkg_prefix}/include
 datarootdir=${install_pkg_prefix}/share
-docdir=${install_pkg_prefix}/doc/
+docdir=${install_pkg_datarootdir}/doc/${PROJECT_NAME}
 
 Name: ${PROJECT_NAME}
 Description: Abstract interface for robot dynamics
--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-aclocal
-automake -ca
-autoconf
+echo ""
+echo "This package is installed through cmake. Please read INSTALL for instructions."
+echo ""
