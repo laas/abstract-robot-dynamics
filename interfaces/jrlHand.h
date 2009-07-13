5479d77df361f3de8c0e8c9d5aecf6cd1d937475
florent <florent@laas.fr>
2009-07-13 15:13:13 +0900

    Clean up abstract interface for hand.
    	* interfaces/jrlHand.h.

--- interfaces/jrlHand.h
+++ interfaces/jrlHand.h
@@ -23,29 +23,100 @@ public:
     /**
     \brief Get the wrist joint to which the hand is attached
     */
-    virtual CjrlJoint* associatedWrist() = 0;
+    virtual const CjrlJoint* associatedWrist() = 0;
+
+  /**
+     \brief Get the center of the hand
+
+     \retval outCenter Center of the hand in the frame of the wrist.
+
+  */
+  virtual void getCenter(vector3d& outCenter) const = 0;
+
+  /**
+     \brief Set the center of the hand
+
+     \param inCenter Center of the hand in the frame of the wrist.
+
+  */
+  virtual void setCenter(const vector3d& inCenter) = 0;
+
+  /**
+     \brief Get thumb axis when had is in open position
+
+     \retval outThumbAxis Axis of the thumb in wrist frame in open position
+
+  */
+  virtual void getThumbAxis(vector3d& outThumbAxis) const = 0;
+
+  /**
+     \brief Set thumb axis in wrist frame when had is in open position
+
+     \param inThumbAxis Axis of the thumb in wrist frame in open position
+  */
+  virtual void setThumbAxis(const vector3d& inThumbAxis) = 0;
+
+  /**
+     \brief Get forefinger axis
+
+     \retval outForeFingerAxis axis of the forefinger in wrist frame 
+     in open position 
+  */
+  virtual void getForeFingerAxis(vector3d& outForeFingerAxis) const = 0;
+
+  /**
+     \brief Set forefinger axis
+
+     \param inForeFingerAxis axis of the forefinger in wrist frame 
+     in open position 
+  */
+  virtual void setForeFingerAxis(const vector3d& inForeFingerAxis) = 0;
+
+  /**
+     \brief Get palm normal
+
+     \retval outPalmNormal normal to the palm in the frame of the wrist.
+  */
+  virtual void getPalmNormal(vector3d& outPalmNormal) const = 0;
+
+  /**
+     \brief Set palm normal
+
+     \param inPalmNormal normal to the palm in the frame of the wrist.
+  */
+  virtual void setPalmNormal(const vector3d& inPalmNormal) = 0;
+
+    /**
+       \name Deprecated methods
+       @{
+    */
 
     /**
     \brief Get the center of the hand in the wrist frame
      */
-    virtual vector3d& centerInWristFrame() = 0;
+    virtual vector3d& centerInWristFrame() __attribute__ ((deprecated)) = 0;
 
+  
     /**
     \brief Get the axis defined by the thumb being held up in the way an "okay" sign is made. The returned axis is a 3d vector in the wrist frame.
      */
-    virtual vector3d& okayAxisInWristFrame() = 0;
+    virtual vector3d& okayAxisInWristFrame() __attribute__ ((deprecated)) = 0;
 
     /**
     \brief Get the axis defined by the forefinger being. The returned axis is a 3d vector in the wrist frame,
      */
-    virtual vector3d& showingAxisInWristFrame() = 0;
+    virtual vector3d& showingAxisInWristFrame() __attribute__ ((deprecated)) = 0;
 
     /**
     \brief Get the axis orthogonal to the palm. The returned axis is a 3d vector in the wrist frame pointing to the direction where all fingers can join,.
      */
-    virtual vector3d& palmAxisInWristFrame() = 0;
+    virtual vector3d& palmAxisInWristFrame() __attribute__ ((deprecated)) = 0;
 
+  /**
+     @}
+  */
 };
+
 
 
 #endif
